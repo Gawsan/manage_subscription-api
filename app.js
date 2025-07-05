219f870e7409825128ae4998efb1ab71a53f7d70
Gawsan <gawsan98@gmail.com>
2025-07-05 22:01:17 +0530

    Implement user authentication with sign-up, sign-in, and sign-out endpoints

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import cookieParser from "cookie-parser";
+import errorMiddleware from "./middleware/error.middleware.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(errorMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,17 +1,10 @@
 import { Router } from "express";
+import { signUp, signIn, signOut } from "../controller/auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", (req, res) => {
-  res.send({ title: "Sign Up", message: "User registration endpoint" });
-});
-
-authRouter.post("/sign-in", (req, res) => {
-  res.send({ title: "Sign In", message: "User login endpoint" });
-});
-
-authRouter.post("/sign-out", (req, res) => {
-  res.send({ title: "Sign Out", message: "User logout endpoint" });
-});
+authRouter.post("/sign-up", signUp);
+authRouter.post("/sign-in", signIn);
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
